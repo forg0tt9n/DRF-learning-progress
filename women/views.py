cfb3cae488afe0ef58bec14be3056311721a513a
forg0tt9n <forg0tt9n@yandex.ru>
2026-01-27 18:16:52 +0700

    урок 3: простой сериализатор

--- women/views.py
+++ women/views.py
@@ -1,24 +1,28 @@
+from django.core.serializers import serialize
 from django.forms.models import model_to_dict
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from django.shortcuts import render
 from rest_framework import generics
-from women.serializer import WomenSerializer
+from women.serializers import WomenSerializer
 from women.models import Women
 
 class WomenAPIView(APIView):
     def get(self, request):
-        women_list = Women.objects.all().values()
-        return Response({'post': list(women_list)})
+        w = Women.objects.all()
+        return Response({'posts': WomenSerializer(w, many=True).data})
 
     def post(self, request):
+        serializer = WomenSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
         post_new = Women.objects.create(
             title = request.data['title'],
             content = request.data['content'],
             cat_id = request.data['cat_id']
         )
-        return Response({'post': model_to_dict(post_new)})
+        return Response({'post': WomenSerializer(post_new).data})
 # Create your views here.
 # class WomenAPIView(generics.ListAPIView):
 #     queryset = Women.objects.all()
