2a0b4ad2784423acd4c2b5263740bf2a3c360d28
forg0tt9n <forg0tt9n@yandex.ru>
2026-01-27 17:31:46 +0700

    урок 2: get и post запросы без сериализатора

--- women/views.py
+++ women/views.py
@@ -1,10 +1,25 @@
+from django.forms.models import model_to_dict
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
 from django.shortcuts import render
 from rest_framework import generics
 from women.serializer import WomenSerializer
 from women.models import Women
 
+class WomenAPIView(APIView):
+    def get(self, request):
+        women_list = Women.objects.all().values()
+        return Response({'post': list(women_list)})
 
+    def post(self, request):
+        post_new = Women.objects.create(
+            title = request.data['title'],
+            content = request.data['content'],
+            cat_id = request.data['cat_id']
+        )
+        return Response({'post': model_to_dict(post_new)})
 # Create your views here.
-class WomenAPIView(generics.ListAPIView):
-    queryset = Women.objects.all()
-    serializer_class = WomenSerializer
+# class WomenAPIView(generics.ListAPIView):
+#     queryset = Women.objects.all()
+#     serializer_class = WomenSerializer
