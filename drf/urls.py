58ad672d36587e39b6dbdce3da83cd65da612b88
forg0tt9n <forg0tt9n@yandex.ru>
2026-01-28 18:22:29 +0700

    урок 9: настойка Viewsets и декоратор action()

--- drf/urls.py
+++ drf/urls.py
@@ -19,8 +19,8 @@ from django.urls import path, include
 from women.views import *
 from rest_framework import routers
 
-router = routers.SimpleRouter()
-router.register(r'women', WomenViewSet)
+router = routers.DefaultRouter()
+router.register(r'women', WomenViewSet, basename='women')
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include(router.urls)),
--- women/views.py
+++ women/views.py
@@ -1,3 +1,4 @@
+from rest_framework.decorators import  action
 from django.core.serializers import serialize
 from django.forms.models import model_to_dict
 from rest_framework.response import Response
@@ -6,11 +7,25 @@ from rest_framework.views import APIView
 from django.shortcuts import render
 from rest_framework import generics, status, viewsets
 from women.serializers import WomenSerializer
-from women.models import Women
+from women.models import Women, Category
+
 
 class WomenViewSet(viewsets.ModelViewSet):
-    queryset = Women.objects.all()
+    # queryset = Women.objects.all()
     serializer_class = WomenSerializer
+
+    def get_queryset(self):
+        pk = self.kwargs.get('pk')
+        if not pk:
+            return Women.objects.all()[:3]
+
+        return Women.objects.filter(pk=pk)
+    @action(methods=['get'], detail=True)
+    def categories(self, request, pk=None):
+
+        cats = Category.objects.get(pk=pk)
+        return Response({'categories': cats.name})
+
 
 #class WomenAPIList(generics.ListCreateAPIView):
 #     queryset = Women.objects.all()
