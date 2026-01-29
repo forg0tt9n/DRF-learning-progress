021736cfa4ae6d3061f97466d0e1e30a38d73017
forg0tt9n <forg0tt9n@yandex.ru>
2026-01-29 12:10:42 +0700

    урок 11: подключение Session-based authentication

--- drf/urls.py
+++ drf/urls.py
@@ -19,14 +19,14 @@ from django.urls import path, include
 from women.views import *
 from rest_framework import routers
 
-#router = routers.DefaultRouter()
-#router.register(r'women', WomenViewSet, basename='women')
+# router = routers.DefaultRouter()
+# router.register(r'women', WomenViewSet, basename='women')
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/women/',WomenAPIList.as_view()),
-    path('api/v1/women/<int:pk>/',WomenAPIUpdate.as_view()),
-    path('api/v1/womendelete/<int:pk>/',WomenAPIDestroy.as_view()),
+    path('api/v1/drf-auth/', include('rest_framework.urls')),
+    path('api/v1/women/', WomenAPIList.as_view()),
+    path('api/v1/women/<int:pk>/', WomenAPIUpdate.as_view()),
+    path('api/v1/womendelete/<int:pk>/', WomenAPIDestroy.as_view()),
 
-
-#    path('api/v1/', include(router.urls)),
+    #    path('api/v1/', include(router.urls)),
 ]
